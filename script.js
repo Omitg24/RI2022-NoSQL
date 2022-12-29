79ccaeb2f87c24d70bab92c2103e17937472b876
uo278968 <uo278968@uniovi.es>
2022-12-29 12:29:59 +0100

    29/12/2022 - David - Visualización grafo

--- script.js
+++ script.js
@@ -2,12 +2,34 @@ class Pokemon {
 
     constructor() {
         this.conectarDriver();
+        var config = {
+            containerId: "viz",
+            neo4j: {
+                serverUrl: "bolt://35.170.182.177:7687",
+                serverUser: "neo4j",
+                serverPassword: "boom-selections-male"
+            },
+            labels: {
+                Person: {
+                    label: "name",
+                    value: "pagerank",
+                    group: "community"
+                }
+            },
+            relationships: {
+                INTERACTS: {
+                    value: "weight"
+                }
+            },
+            initialCypher: "MATCH (n:Person{name:'Jincho'}) RETURN n"
+        };
+
+        this.viz = new NeoVis.default(config);
     }
 
     async conectarDriver() {
         const driver = neo4j.driver("bolt://35.170.182.177:7687"
             , neo4j.auth.basic("neo4j", "boom-selections-male"))
-
         try {
             await driver.verifyConnectivity();
             console.log('Driver created');
@@ -19,52 +41,34 @@ class Pokemon {
     }
 
     async consulta1() {
-        try {
-            var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
-            console.log(result);
-        } finally {
-            await this.session.close();
-        }
+        var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
+        console.log(result);
+        this.viz.renderWithCypher("MATCH (n:Person{name:'Foyone'}) RETURN n ");
+        this.viz.reload();
     }
 
     async consulta2() {
-        try {
-            var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
-        } finally {
-            await this.session.close();
-        }
+
+        var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
+        this.viz.renderWithCypher("MATCH (n:Person{name:'Jincho'}) RETURN n ");
+        this.viz.reload();
     }
 
     async consulta3() {
-        try {
-            var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
-        } finally {
-            await this.session.close();
-        }
+        var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
     }
 
     async consulta4() {
-        try {
-            var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
-        } finally {
-            await this.session.close();
-        }
+        var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
     }
 
     async consulta5() {
-        try {
-            var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
-        } finally {
-            await this.session.close();
-        }
+        var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
+
     }
 
     async consulta6() {
-        try {
-            var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
-        } finally {
-            await this.session.close();
-        }
+        var result = await this.session.run("MATCH (n:Person{name:'Foyone'}) RETURN n ");
     }
 }
 
